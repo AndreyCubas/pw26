be35027d5bc01fc8ef0abb59a285ab6c15f6e3b2
AndreyCubas <felipedreycubascardoso@gmail.com>
2026-05-21 08:30:18 -0300

    feat: adiciona configuração do STATIC_ROOT e atualiza dependências

--- pw2026/settings.py
+++ pw2026/settings.py
@@ -154,6 +154,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static_gcloud"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
